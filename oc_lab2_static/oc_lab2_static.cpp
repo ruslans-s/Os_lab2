2b409892184c6f2ead92a6c34e01f783096c4fe3
RS <sadikov.tj@yandex.ru>
2021-03-26 22:30:36 +0300

    исправил поиск в реестре

--- oc_lab2_static/oc_lab2_static.cpp
+++ oc_lab2_static/oc_lab2_static.cpp
@@ -9,6 +9,7 @@
 #include <regex>
 #include "atlconv.h" 
 
+
 using namespace std;
 
 typedef double (*function)(double a, double b);
@@ -80,17 +81,13 @@ void getWayDllFromReg() {
     RegCloseKey(hKey);
 }
 
-void setWayToDll(string Way) {
+void setWayToDll(char strt[256]) {
   
     TCHAR fileDll[64];
     HKEY hKey;
     DWORD dwDisposition;
     
-    char strt[256];
-      
-    for (int i = 0; i < Way.size(); i++) {
-        strt[i] = Way[i];
-    }
+    
 
     TCHAR Reget[256] = { 0 };
     DWORD RegetPath = sizeof(Reget);
@@ -103,13 +100,18 @@ void setWayToDll(string Way) {
     }
     else
     {
-            if (RegSetValueEx(hKey, TEXT("String Value"), NULL, REG_SZ, (BYTE*)strt, strlen(strt)) == ERROR_SUCCESS)
+            if (RegSetValueEx(hKey, TEXT("Way to Dll"), NULL, REG_SZ, (BYTE*)strt, strlen(strt)) == ERROR_SUCCESS)
             {
                 cout << "путь записан";
             } 
         }
  
     RegCloseKey(hKey);
+}
+
+std::string make_string(LPBYTE data, int size)
+{
+    return std::string(reinterpret_cast<char*>(data), reinterpret_cast<char*>(data + size));
 }
 
 int main()
@@ -121,12 +123,12 @@ int main()
     std::wstring stemp;
     HMODULE hm = 0;
 
-    std::string fileWay;
+   // std::string fileWay;
 
     TCHAR fileDll[64];
     HKEY hKey;
     DWORD dwDisposition;
-    char strt[256];
+    char fileWay[256];
     TCHAR Reget[256] = { 0 };
     DWORD RegetPath = sizeof(Reget);
     TCHAR chekDll();
@@ -143,14 +145,19 @@ resetChekWay:
         else
         {
             //Проверяем файл
-            if ((RegQueryValueEx(hKey, TEXT("Way to Dll"), NULL, NULL, (LPBYTE)&Reget, &RegetPath)) == ERROR_SUCCESS)
-            {
+            if ((RegQueryValueEx(hKey, TEXT("Way to Dll"), NULL, NULL, (LPBYTE)&fileWay, &RegetPath)) == ERROR_SUCCESS)  {
                 printf("Найденный путь к DLL");
+
                 printf("%s", Reget);
-                hm = LoadLibrary(Reget);
+               
+                stemp = s2ws(fileWay);
+                result = stemp.c_str();
+                hm = LoadLibrary(result);
+              
 
-                
-
+            }
+            else {
+                printf("%s", Reget);
             }
         }
         RegCloseKey(hKey);
@@ -164,8 +171,7 @@ resetChekWay:
         goto resetChekWay;
         }
 
-
-        while (hm == NULL)
+  while (hm == NULL)
         {
 
             printf("Ошибка загрузки Dll\n");
@@ -178,7 +184,7 @@ resetChekWay:
             hm = LoadLibrary(result);
             setWayToDll(fileWay);
         }
-    
+   
     
         function fx = (function)GetProcAddress(hm, "func");
        
@@ -208,14 +214,3 @@ resetChekWay:
     
 
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
